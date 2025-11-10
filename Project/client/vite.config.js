683cc4e4385abe0bc42e0e9e78cec4b79fcf1cad
gwrig007 <gwrig007@odu.edu>
2025-11-09 20:59:24 -0500

    fixing vite.config and 502 gateway error

--- Project/client/vite.config.js
+++ Project/client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  root: ".", // current folder
+  build: {
+    outDir: "dist", // build output
+  },
+  server: {
+    port: 5173,
+  },
+});
