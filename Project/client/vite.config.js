0fd8b7499640aaa72c6832e4a586858d6b1e7061
gwrig007 <gwrig007@odu.edu>
2025-11-09 21:43:19 -0500

    fixing dist folder

--- Project/client/vite.config.js
+++ Project/client/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  root: ".", // current folder
+  root: ".", // Keep current folder as root
   build: {
-    outDir: "dist", // build output
+    outDir: "dist", // Output folder
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        signin: resolve(__dirname, "signin.html"),
+        profile: resolve(__dirname, "profile.html"),
+        verifyOtp: resolve(__dirname, "verify-otp.html"),
+        forgot: resolve(__dirname, "forgot.html"),
+        home: resolve(__dirname, "home.html"),
+      },
+    },
   },
   server: {
     port: 5173,
